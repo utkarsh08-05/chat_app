834f32e407fee0f851e38a015573ef7c46aef594
Utkarsh <your_github_email@example.com>
2025-12-17 01:50:27 +0530

    Fix Render Django path

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Current directory:"
+pwd
+ls -la
+
+export DJANGO_SETTINGS_MODULE=vartalap.settings
+export PYTHONPATH=/opt/render/project/src
+
 pip install -r requirements.txt
+
 python manage.py migrate
 python manage.py collectstatic --noinput
